701951edc1ba51dd28aec090d01026bb5aba7491
joydeep-bhowmik <bhowmikj9432@gmail.com>
2024-09-28 12:40:12 +0530

    helpers.php removed

--- tests/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-function refreshDatabase()
-{
-    // Drop all tables
-    Capsule::schema()->dropAllTables();
-
-    // Recreate tables
-    require __DIR__ . '/migrations/create_tables.php';
-}
